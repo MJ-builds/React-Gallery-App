61df7755be7b1aac33f3d1a9ae5594d5d86f9da2
MJ <112978328+MJ-builds@users.noreply.github.com>
2023-03-15 17:55:26 +0200

    update

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -3,6 +3,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div className="not-found">
+    <div className= 'divider'></div>
       <h3>No Results Found</h3>
       <p>Your search did not return any results. Please try again.</p>
     </div>
